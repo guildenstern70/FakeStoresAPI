1530b4cf7c4695603a83998b76aa95755c0abb04
guildenstern70 <alessiosaltarin@gmail.com>
2018-08-14 10:23:07 +0200

    Added Google Verify

--- routes/api.js
+++ routes/api.js
@@ -3,78 +3,80 @@
 const express = require('express');
 const router = express.Router();
 
-const reports = {
-    title: 'Reports',
-    items: [
-        {
-            id: 0,
-            created: '10-gen-2018',
-            name: 'Report 831029',
-            isComplete: true
-        },
-        {
-            id: 1,
-            created: '11-gen-2018',
-            name: 'Report 831030',
-            isComplete: false
-        },
-        {
-            id: 2,
-            created: '10-feb-2018',
-            name: 'Report 831031',
-            isComplete: false
-        },
-        {
-            id: 3,
-            created: '14-mar-2018',
-            name: 'Report 831032',
-            isComplete: false
-        }
-    ]
-};
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
 
 /* GET home page. */
 router.get('/version', function (req, res) {
 
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({version: "0.1.0"}));
+    res.send({version: "0.1.0"});
+});
+
+/* GET google verify.
+   Example:
+   GET /api/google/verify/applications/packageAlessio/purchases/products/00001/tokens/2180183832218021?access_token=38032
+ */
+router.get('/google/verify/applications/:packagename/purchases/products/:productid/tokens/:token', function (req, res) {
+
+    const accessToken = req.query.access_token;
+
+    if (isEmpty(accessToken)) {
+        const msg = "Unauthorized";
+        console.error(msg);
+        res.status(401).send(msg);
+        return;
+    }
+
+    console.log("Access Token = " + accessToken);
+
+    if (isEmpty(req.params.packagename)) {
+        const msg = "Package name is null or empty";
+        console.error(msg);
+        res.status(400).send(msg);
+        return;
+    }
+
+    if (isEmpty(req.params.productid)) {
+        const msg = "Product ID is null or empty";
+        console.error(msg);
+        res.status(400).send(msg);
+        return;
+    }
+
+    if (isEmpty(req.params.token)) {
+        const msg = "Token is null or empty";
+        console.error(msg);
+        res.status(400).send(msg);
+        return;
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+
+    console.log("Package Name = " + req.params.packagename);
+    console.log("Product ID = " + req.params.productid);
+    console.log("Token = " + req.params.token);
+
+    const googleResponse = {
+        "kind": "androidpublisher#productPurchase",
+        "purchaseTimeMillis": Date.now(),
+        "purchaseState": 0, // 0=purchased, 1=cancelled
+        "consumptionState": 0, // 0=to be consumed, 1=consumed
+        "developerPayload": "This is the developer payload",
+        "orderId": "3289HJHHK987393J",
+        "purchaseType": 1 // 0=test, 1=promo
+    };
+
+    res.send(googleResponse);
 });
 
 /* GET reports. */
-router.get('/reportsdata', function (req, res) {
+router.get('/apple/verify/', function (req, res) {
 
     res.setHeader('Content-Type', 'application/json');
-    res.send(reports);
+    res.send({"todo": true});
 });
 
-/* POST new report. */
-router.post('/reportsdata', function (req, res) {
-
-    console.log('POST Received ' + JSON.stringify(req.body));
-
-    reports.items.push( {
-        id: reports.items.length+1,
-        created: req.body.date,
-        name: req.body.name,
-        isComplete: (req.body.complete=='true')
-    });
-
-    res.status(201).send(); // Created
-
-});
-
-/* DELETE existing report */
-router.delete('/reportsdata', function (req, res) {
-
-    const id = Number(req.query.id);
-    for (let idx in reports.items) {
-        const item = reports.items[idx];
-        if (item.id === id) {
-            console.log('Deleting item at ' + idx);
-            delete reports.items[idx];
-        }
-    }
-    res.status(204).send(); // No-Content
-});
 
 module.exports = router;
